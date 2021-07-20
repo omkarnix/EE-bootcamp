6874ff0f2ae889162cc0502376b48b9515165112
Omkar Karmarkar <omkarkk01@gmail.com>
2021-07-20 23:10:25 +0530

    implement multiple filters - odd prime

--- app/src/main/java/org/agileindia/mathworks/Filter.java
+++ app/src/main/java/org/agileindia/mathworks/Filter.java
@@ -5,10 +5,17 @@ import java.util.List;
 
 public class Filter {
 
-    public static List<Integer> select(Condition condition, List<Integer> numbers) {
+    public static List<Integer> select(List<Condition> conditions, List<Integer> numbers) {
         List<Integer> selected = new ArrayList<>();
         for (int number : numbers) {
-            if (condition.isSatisfiedBy(number)) {
+            boolean allConditionsSatisfied = true;
+            for (Condition condition : conditions) {
+                if (!condition.isSatisfiedBy(number)) {
+                    allConditionsSatisfied = false;
+                    break;
+                }
+            }
+            if (allConditionsSatisfied) {
                 selected.add(number);
             }
         }
--- app/src/test/java/org/agileindia/mathworks/FilterSpec.java
+++ app/src/test/java/org/agileindia/mathworks/FilterSpec.java
@@ -1,6 +1,7 @@
 package org.agileindia.mathworks;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
@@ -15,11 +16,11 @@ public class FilterSpec {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, -1, -3);
 
         //When
-        List<Integer> primeNumbers = Filter.select(Condition.prime, numbers);
+        List<Integer> primeNumbers = Filter.select(Collections.singletonList(Condition.prime), numbers);
 
         //Then
         assertThat(primeNumbers, hasSize(2));
-        assertThat(primeNumbers, containsInAnyOrder(2,3));
+        assertThat(primeNumbers, containsInAnyOrder(2, 3));
     }
 
     @Test
@@ -28,10 +29,22 @@ public class FilterSpec {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, -1);
 
         //When
-        List<Integer> oddNumbers = Filter.select(Condition.odd, numbers);
+        List<Integer> oddNumbers = Filter.select(Collections.singletonList(Condition.odd), numbers);
 
         //Then
         assertThat(oddNumbers, hasSize(3));
         assertThat(oddNumbers, containsInAnyOrder(1, 3, -1));
+    }
+
+    @Test
+    public void itSelectsOddPrimes() {
+        //Given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, -1, -2, -3);
+
+        //When
+        List<Integer> oddPrimes = Filter.select(Arrays.asList(Condition.odd, Condition.prime), numbers);
+
+        //Then
+        assertThat(oddPrimes, containsInAnyOrder(3, 5, 7));
     }
 }
